3c0f672b3a834f11c8af1e5789645e7c26fe51ed
L-Naej <jn.chiganne@gmail.com>
2014-04-03 20:24:12 +0200

    First commit

---
+++ src/game/Tetraword.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ * Main class of Tetraword game.
+ * @author L-Naej
+ *
+ */
+public class Tetraword {
+
+  public static void main(String[] args) {
+    System.out.println("Nothing implemented yet.");
+
+  }
+
+}
